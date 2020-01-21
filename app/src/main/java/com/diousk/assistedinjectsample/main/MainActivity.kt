09957ab55cc2c5921ccd06c184d98e1020e5958b
David Chen <diousk507@gmail.com>
2020-01-21 22:54:20 +0800

    test SaveInstanceState is not related to SavedStateHandle

--- app/src/main/java/com/diousk/assistedinjectsample/main/MainActivity.kt
+++ app/src/main/java/com/diousk/assistedinjectsample/main/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.diousk.assistedinjectsample.main
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProviders
 import com.diousk.assistedinjectsample.R
 import com.diousk.assistedinjectsample.di.viewmodel.DaggerAwareViewModelFactory
 import dagger.android.support.DaggerAppCompatActivity
+import timber.log.Timber
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -16,5 +18,17 @@ class MainActivity : DaggerAppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel.fetchUser()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Timber.d("onSaveInstanceState, save userA to mainA")
+        outState.putString("userA", "mainA")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val initPosition = savedInstanceState.getString("userA")
+        Timber.d("onRestoreInstanceState, restore userA $initPosition")
     }
 }
